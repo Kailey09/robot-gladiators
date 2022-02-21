ff061c3ea2af91194991a1e3183766643d3a92d8
Kailey09 <kcoop1323@gmail.com>
2022-02-21 16:11:45 -0500

    longer rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,7 +20,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -65,9 +66,11 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
           fight();
       }
   }
-
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
