c18eaed732a22e5126cf80bd466d2dd6567de68f
Kailey09 <kcoop1323@gmail.com>
2022-02-21 17:50:11 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,14 +67,32 @@ var fight = function(enemyName) {
             window.alert(playerName + 'has died!');
             // leave while() loop if player is dead
             break;
-        } else {
+        }else {
             window.alert(playerName + ' still has' + playerHealth + ' health left.');
         }
     }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember arrays start at 0 so it needs a 1 added to it
+        window.alert("Welcome to Robot-Galidators! Round" + ( i + 1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        //use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+
+        //pass the pickedEnemyName variable's value into the fight functions, where it wil assume the value of the enemyName parameter
+        fight(pickedEnemyName); 
+    }  
+    // if player isn't alive stop game
+    else {
+        window.alert("You have lost your robot in battle! Game Over! ");
+        break;
+    }
 }
